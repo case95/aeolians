8db50477756c5e6e8ca99ba6d3afaf66da9566f4
case95 <lorenzodr.design@gmail.com>
2020-12-11 11:01:18 +0100

    firebase project id fix

--- src/config/db.js
+++ src/config/db.js
@@ -8,16 +8,19 @@ const dbaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
   authDomain: process.env.REACT_APP_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE,
-  projectId: "aeolians-86d4b" /*process.env.REACT_APP_PROJECT_ID*/,
+  projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
 };
+
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(dbaseConfig);
+
 // Intialize Firestore
 firebaseApp.firestore();
 
 const firebaseStorage = firebase.storage();
 
+export const auth = firebaseApp.auth();
 export { firebaseApp, firebaseStorage };
