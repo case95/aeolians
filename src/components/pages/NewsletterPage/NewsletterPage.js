2be3236137614b2fee5fb4ec8f77895aa0c6acb3
case95 <lorenzodr.design@gmail.com>
2020-12-19 11:57:08 +0100

    NewslettePage needs fixes in auth

--- src/components/pages/NewsletterPage/NewsletterPage.js
+++ src/components/pages/NewsletterPage/NewsletterPage.js
@@ -4,6 +4,8 @@ import { compose } from "redux";
 import { connect } from "react-redux";
 import { withFirestore, useFirestoreConnect } from "react-redux-firebase";
 
+import { Row, Col } from "react-bootstrap";
+
 import PageTitle from "../../customComponents/PageTitle/PageTitle";
 
 const NewsletterPage = (props) => {
@@ -13,6 +15,7 @@ const NewsletterPage = (props) => {
   const [emailList, setEmailList] = useState([""]);
 
   useEffect(() => {
+    console.log("EMAILS", emails);
     emails &&
       emails.map((email, index) => {
         const newArray = emailList;
@@ -29,7 +32,19 @@ const NewsletterPage = (props) => {
         undertitle="Copy the emails from this list and paste them in your email recipient."
         className="mb-3"
       />
-      <p className="text-center">{emailList.join(", ")}</p>
+      {emails ? (
+        <p className="text-center">{emailList.join(", ")}</p>
+      ) : (
+        <Row>
+          <Col className="text-center">
+            <p className="m-0 d-inline-block">
+              <div className="spinner-border text-primary" role="status">
+                <span className="sr-only">Loading...</span>
+              </div>
+            </p>
+          </Col>
+        </Row>
+      )}
     </div>
   );
 };
